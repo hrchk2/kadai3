c8804a481222befdf5d6663f880902c3af3e2776
EC2 Default User <amrumineq@gmail.com>
2022-10-16 10:14:42 +0000

    books showまで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,8 +18,10 @@ class BooksController < ApplicationController
 
   def show
    @book = Book.find(params[:id])
+   @user = User.find(params[:id])
+   @booknew =Book.new
   end
-
+   
   def edit
   end
 
