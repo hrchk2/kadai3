3649e05b3c00206e9017363270d6351fd68190cd
EC2 Default User <amrumineq@gmail.com>
2022-10-17 03:12:05 +0000

    user_updateまで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,9 @@ class UsersController < ApplicationController
   end
   
   def update
-     @user = User.find(params[:id])
-     @user.update(user_params)
-     redirect_to user_path(@user.id)
+     user = User.find(params[:id])
+     user.update(user_params)
+     redirect_to user_path(user.id)
   end
   
   def show
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:profile_image, :name, :email)
+    params.require(:user).permit(:profile_image, :name, :email,:introduction)
   end
 
 
