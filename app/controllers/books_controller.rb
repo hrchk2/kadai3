9f3acd20ab8f96f5df01f4ebcd650949b6cbb026
EC2 Default User <amrumineq@gmail.com>
2022-10-17 06:05:24 +0000

    バリデーションまで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,13 @@ class BooksController < ApplicationController
   def create
     @booknew=Book.new(book_params)
     @booknew.user_id = current_user.id
-    @booknew.save
-    redirect_to book_path(@booknew.id)
+    @user = current_user
+    if @booknew.save
+       redirect_to book_path(@booknew.id),notice:"You have created book successfully."
+    else
+      @books =Book.all.order(id: :asc)
+      render :index
+    end
   end
 
   def index
@@ -27,9 +32,12 @@ class BooksController < ApplicationController
   end
 
   def update
-   book=Book.find(params[:id])
-   book.update(book_params)
-   redirect_to book_path(book.id)
+    @book=Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id),notice:"You have updated book successfully."
+    else
+      render :edit
+    end
   end
 
   def destroy
